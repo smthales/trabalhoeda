95dd2e3e0776bcf7f19eacda966169da271cd32e
Thales <thalesabranchess@gmail.com>
2023-12-02 15:52:14 -0300

    construindo estruturas

--- netflix.c
+++ netflix.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct chavePrimaria {
 
+  char tituloFilme[82];
+  int anoLancamentoFilme;
 
+} TCP;
 
+typedef struct informacaoSubordinada {
 
-int main() {
-    
-    
-    
-    return 0;
+  char nomeDiretor[52];
+  char genero[32];
+  int duracaoMinutos;
+
+} TIS;
+
+typedef struct arvb {
+
+  struct chavePrimaria **chavesPrimarias;
+  struct informacaoSubordinada **informacoesSubordinadas;
+  struct arvb **filho;
+  int folha, nchaves;  
+  
+} TARVB;
+
+int main(void) {
+  printf("Hello World\n");
+  printf("ola");
+  return 0;
 }
